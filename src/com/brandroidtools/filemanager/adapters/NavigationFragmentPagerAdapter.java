3315ce62945ea001a969e2476247e5e2bd4122ba
Brent Kensey <bkensey@gmail.com>
2013-03-02 13:52:03 -0500

    Fix license headers

--- src/com/brandroidtools/filemanager/adapters/NavigationFragmentPagerAdapter.java
+++ src/com/brandroidtools/filemanager/adapters/NavigationFragmentPagerAdapter.java
@@ -1,13 +1,21 @@
-package com.brandroidtools.filemanager.adapters;
-
-/**
- * Created with IntelliJ IDEA.
- * User: Brent
- * Date: 1/16/13
- * Time: 9:41 PM
- * To change this template use File | Settings | File Templates.
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ * Copyright (C) 2013 BrandroidTools
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
+package com.brandroidtools.filemanager.adapters;
 
 import android.content.Context;
 import android.os.Bundle;
