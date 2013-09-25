17141a5c2286b7e2acc1c6e19f3bfcb74b26ebbd
Brandon Bowles (Brandon5) <BrandonBowles@gmail.com>
2013-09-25 14:13:14 -0700

    Only remove user-defined bookmarks if selection fails

--- Backbone/src/main/java/me/toolify/backbone/activities/NavigationActivity.java
+++ Backbone/src/main/java/me/toolify/backbone/activities/NavigationActivity.java
@@ -982,7 +982,8 @@ public class NavigationActivity extends AbstractNavigationActivity
     @Subscribe
     public void onBookmarkOpen(BookmarkOpenEvent event) {
     	String path = event.path;
-    	// Check that the bookmark exists
+        Bookmark b = Bookmarks.getBookmark(getContentResolver(), path);
+        // Check that the bookmark exists
         try {
             FileSystemObject fso = CommandHelper.getFileInfo(this, path, null);
             if (fso != null) {
@@ -991,10 +992,12 @@ public class NavigationActivity extends AbstractNavigationActivity
             } else {
                 // The bookmark not exists, delete the user-defined bookmark
                 try {
-                	BusProvider.postEvent(new BookmarkDeleteEvent(path));
-                	Bookmark b = Bookmarks.getBookmark(getContentResolver(), path);
-                    Bookmarks.removeBookmark(this, b);
-                    BusProvider.postEvent(new BookmarkRefreshEvent());
+                    if(b.getType() == Bookmark.BOOKMARK_TYPE.USER_DEFINED)
+                    {
+                        BusProvider.postEvent(new BookmarkDeleteEvent(path));
+                        Bookmarks.removeBookmark(this, b);
+                        BusProvider.postEvent(new BookmarkRefreshEvent());
+                    }
                 } catch (Exception ex) {/**NON BLOCK**/}
             }
         } catch (Exception e) {
@@ -1003,7 +1006,8 @@ public class NavigationActivity extends AbstractNavigationActivity
             if (e instanceof NoSuchFileOrDirectory || e instanceof FileNotFoundException) {
                 // The bookmark not exists, delete the user-defined bookmark
                 try {
-                	BusProvider.postEvent(new BookmarkDeleteEvent(path));
+                    if(b.getType() == Bookmark.BOOKMARK_TYPE.USER_DEFINED)
+                	    BusProvider.postEvent(new BookmarkDeleteEvent(path));
                 } catch (Exception ex) {/**NON BLOCK**/}
             }
             return;
